757f020bd6021481fa0c2a7de2f2dd9e8d1fe20f
AdriaSalvado <36168551+AdriaSalvado@users.noreply.github.com>
2018-02-09 12:55:43 +0100

    Add files via upload

---
+++ ejercicio-mayor_menor.py
@@ -0,0 +1,11 @@
+# coding: utf8
+# Adrià Salvadó
+
+A=input ("introduce un número")
+B=input ("introduce un número")
+C=input ("introduce un número")
+
+if (A==B) and (A==C)
+	print "los tres numeros son iguales"
+else:
+	if (B==C
