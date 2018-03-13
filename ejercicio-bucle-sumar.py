5c0da8455e00f6f4b159dfd4416be61e2fc62d36
AdriaSalvado <36168551+AdriaSalvado@users.noreply.github.com>
2018-03-13 13:41:45 +0100

    Update ejercicio-bucle-sumar.py

--- ejercicio-bucle-sumar.py
+++ ejercicio-bucle-sumar.py
@@ -1,18 +1,19 @@
 # coding: utf8
-# Adrià Salvadó
 # Inicializaciones
-
 salir = "N"
-suma=1
-resultado=0
-
+numero=1
+maximo=5
+suma=0
 while ( salir=="N" ):
     # Hago cosas
-    print resultado
-
+    print numero ,
+    if(numero<=maximo-1):
+		print "+" ,
+	
+    suma=suma+numero
     # Incremento
-    resultado=resultado + suma
-    suma=suma+1
+    numero=numero+1
     # Activo indicador de salida si toca
-    if ( resultado > 15): # Condición de salida
+    if ( numero > maximo ): # Condición de salida
             salir = "S"
+print "=" , suma 
